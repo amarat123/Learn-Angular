ba6f80b3d962855a812d86a532da2b80d10be97b
DESKTOP-FIK5SPH\amarat <amarat@saglus.com>
2022-09-06 18:48:23 +0530

    work on contact from

--- blogger/src/app/app.module.ts
+++ blogger/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -33,6 +34,8 @@ import { httpInterceptorProviders } from './http-interceptors/index';
     CmspageModule,
     AdminModule,
     AuthModule,
+
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [Title, httpInterceptorProviders],
--- blogger/src/app/cmspage/contact-form/contact-form.component.ts
+++ blogger/src/app/cmspage/contact-form/contact-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Contact } from '../contact';
 import { CmspageService } from '../cmspage.service'
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact-form',
@@ -10,15 +12,56 @@ import { Router } from '@angular/router';
 })
 export class ContactFormComponent implements OnInit {
 
-  error = {};
+  //model = new Contact();
+  model : any;
+  submitted = false;
+  error: any = {};
+
+  signUpForm: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     private routet : Router,
     private cmspageService : CmspageService,
     //private model : Contact
   ) { }
 
+
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+
+    this.signUpForm = this.fb.group({
+      first_name: ['', [
+          Validators.required,
+          Validators.maxLength(100)
+      ]]
+      /*
+      email: ['', [
+          Validators.required,
+          Validators.maxLength(255),
+          Validators.minLength(3),
+          Validators.email
+      ]
+      ],*/
+      
+    });
+
+
+
+    return this.cmspageService.contactForm(this.model).subscribe({
+        next : (res) => {
+          this.model = res
+        },
+        error : (err) => {
+          this.error = err
+        },
+        complete : () => {},
+      }
+    );
   }
 
   gotoHome(){
